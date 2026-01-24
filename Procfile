c864522532d9e2c7405b586636b9da209c2195c0
Laiba Javed <laibajaved@punch.agency>
2026-01-24 19:24:04 +0500

    Optimize for high concurrency: Switch to ASGI/Uvicorn and enable DB connection pooling

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn ttscanner_backend.wsgi:application --timeout 120 --workers 2 --threads 1 --bind 0.0.0.0:$PORT
+web: gunicorn ttscanner_backend.asgi:application -k uvicorn.workers.UvicornWorker --workers 4 --threads 4 --timeout 120 --bind 0.0.0.0:$PORT
 worker: celery -A ttscanner_backend worker --loglevel=info --concurrency=1 --maxtasksperchild=3
 beat: celery -A ttscanner_backend beat --loglevel=info
--- ttscanner_backend/asgi.py
+++ ttscanner_backend/asgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
-import os
+# import os
 
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ttscanner_backend.settings')
+
+# application = get_asgi_application()
+import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ttscanner_backend.settings')
 
-application = get_asgi_application()
+# This is the standard entry point
+django_asgi_app = get_asgi_application()
+
+application = django_asgi_app
